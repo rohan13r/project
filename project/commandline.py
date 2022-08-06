2188d46722d33c94248defafea6ac3bc9ac76d8d
rohan13r <96933737+rohan13r@users.noreply.github.com>
2022-08-07 03:37:25 +0530

    Update commandline.py

--- project/commandline.py
+++ project/commandline.py
@@ -88,68 +88,75 @@ while True:
                 print("Welcome "+str(n))
                 while True:
                     print("Press :")
+                    
                     print("1: Create Products")
                     print("2: View all Products created")
                     print("3: View Product Details by ID")
                     print("4: Update Product")
+                    print('5: View User Profile')
                     print("0: Logout")
                     try:
                         i1=int(input())
                     except ValueError:
                         print("Please enter valid choice")
                     if i1==1:
-                        Ev_ID=operations.AutoGenerate_prodID()
-                        print("prod ID Generated - "+str(Ev_ID))
-                        print("Enter prod Name:")
+                        prodId=operations.AutoGenerate_prodID()
+                        print("prod ID Generated - "+str(prodId))
+                        print("Product Title")
                         prodtitle=input()
-                        print("Enter prod Type:")
+                        print("Product Type")
                         prodtype=input()
-                        print("Enter Price per day:")
-                        prodprice=input()
-                        print("Enter Total Stock:")
-                        available=input()
+                        print("Price Per Day")
+                        prodprice = input()
+                        print("Total Stock Available")
+                        available = input()
                         try:
-                            operations.Create_prod(n,'prod.json',prodId, prodtitle, prodtype,prodprice,available)
-                            print("prod created successfully")
+            
+
+                            Cap=int(input())
                         except ValueError:
                             print("Please enter valid data")
                             continue
+                        else:
+                            operations.Create_Product(n,'Products.json',prodId, prodtitle, prodtype,prodprice,available)
+                            print("prod created successfully")
                     elif i1==2:
-                        pd_details=operations.View_prods(n,'prod.json')
-                        if len(ev_details)==0:
+                        pd_details=operations.View_all_products_of_seller(n,'Products.json')
+                        print(pd_details)
+                        if len(pd_details)==0:
                             print("No prods created yet! \n")
                         else:
                             for i in range(len(pd_details)):
                                 print("Product ID: "+str(pd_details[i]['ProductID']))
                                 print("Product Name: " +str(pd_details[i]['Product Title']))
-                                print("Seller: " +str(pd_details[i]['seller']))
+                                print("Seller: " +str(pd_details[i]['Seller']))
                                 print("Seats Available: "+str(pd_details[i]['Total Stock Available']))
                                 print('\n')
                     elif i1==3:
                         print("Enter Product ID")
-                        pd_id=input()
-                        f14=open('prod.json','r')
+                        prodId=input()
+                        f14=open('Products.json','r')
                         try:
                             c14=str(json.load(f14))
-                            if pd_id not in c14:
+                            if prodId not in c14:
                                 print("Invalid prod ID")
                                 continue
                         except JSONDecodeError:
                             print("prods not available")
                             continue
-                        d=operations.View_prod_ByID('prod.json',pd_id)
-                        print("prod Name: " +str(d[0]['Product Title']))
-                        print("seller: " +str(d[0]['seller']))
-                        print("Seats Available: "+str(d[0]['Total Stock Available']))
+                        d=operations.View_prod_ByID('Products.json',prodId)
+                        print("prod Title: " +str(d['Product Title']))
+                        print("seller: " +str(d['Seller']))
+                        print("Seats Available: "+str(d['Total Stock Available']))
                         print('\n')
                     elif i1==4:
                         print("Enter prod ID:")
-                        ev_id=input()
-                        print("Enter detail to be Updated ( Title || Type || Price per day): ")
+                        prodId=input()
+                        print("Enter detail to be Updated ( Product Title || Product Type || Price Per Day || Total Stock Available ): ")
                         dtl=input()
-                        print("Enter new value:")
+                        print('Update to:')
                         updtl=input()
-                        f11=open('prod.json','r')
+                        f11=open('Products.json','r')
                         try:
                             c11=str(json.load(f11))
                             f11.close()
@@ -157,41 +164,18 @@ while True:
                             print("No prods created")
                             f11.close()
                             continue
-                        if ev_id not in c11:
+                        if prodId not in c11:
                             print("Invalid prod ID")
                             continue
-                        if (len(ev_id)*len(dtl)*len(updtl))==0:
+                        if (len(prodId)*len(dtl)*len(updtl))==0:
                             print("Please enter valid data")
                             continue
                         else:
-                            ch=operations.Update_product(n,'prod.json',ev_id,dtl,updtl)
+                            ch=operations.Update_product(n,'Products.json',prodId,dtl,updtl)
                         if ch==False:
                             print("Cannot update prod")
                         else:
                             print("prod updated successfully")
-                    elif i1==5:
-                        print("Enter prod ID")
-                        prod_id=input()
-                        f1=open('prod.json','r')
-                        try:
-                            c1=str(json.load(f1))
-                            f1.close()
-                        except JSONDecodeError:
-                            print("No prods created")
-                            f1.close()
-                            continue
-                        if prod_id not in c1:
-                            print("Invalid prod ID")
-                            continue
-                        if len(prod_id)==0:
-                            print("Please enter valid data")
-                            continue
-                        else:
-                            o1=operations.Delete_prod(n,'prod.json',prod_id)
-                        if o1==True:
-                            print("prod deleted successfully")
-                        else:
-                            print("Cannot delete prod")
                     elif i1==0:
                         break
                     else:
@@ -217,77 +201,88 @@ while True:
                 print("Welcome "+str(n))
                 while True:
                     print("Press:")
-                    print("1: View Registered prods")
-                    print("2: Register for an prod")
-                    print("3: Update Password")
-                    print("4: View prod Details by ID")
+                    print("1: View all Product")
+                    print("2: Manage wishlist")
+                    print("3: Manage cart")
+                    print("4: Place order")
+                    print("5: Update Profile")
+                    print("6: View Orders")
                     print("0: Logout")
                     try:
                         i=int(input())
                     except ValueError:
                         print("Please enter valid choice")
                     if i==1:
-                        all_prods=[]
-                        upcoming_ongoing=[]
-                        operations.fetch_all_prods('prod.json',n,all_prods,upcoming_ongoing)
-                        print("All Upcoming/Ongoing prods: ")
-                        for i in range(len(upcoming_ongoing)):
-                            print("prod Name: " +str(upcoming_ongoing[i]['Name']))
-                            print("seller: " +str(upcoming_ongoing[i]['seller']))
-                            print('\n')
-                    elif i==2:
-                        l=operations.View_all_products('prod.json')
-                        if len(l)==0:
-                            print("No prods available")
+                        pd_details=operations.View_all_products('Products.json')
+                        print(pd_details)
+                        if len(pd_details)==0:
+                            print("No prods created yet! \n")
                         else:
-                            print("All prods: ")
-                            for i in range(len(l)):
-                                t=l[i]
-                                print("prod ID: "+str(t['ID']))
-                                print("prod Name: " +str(t['Name']))
-                                print("Seller: "+str(t['seller']))
-                                print("Seats Available: "+str(t['Seats Available']))
-                                print("Total Seats: "+str(t['Capacity']))
+                            for i in range(len(pd_details)):
+                                print("Product ID: "+str(pd_details[i]['ProductID']))
+                                print("Product Name: " +str(pd_details[i]['Product Title']))
+                                print("Seller: " +str(pd_details[i]['Seller']))
+                                print("Seats Available: "+str(pd_details[i]['Total Stock Available']))
                                 print('\n')
-                        print("Enter prod ID: ")
-                        prod_id=input()
-                        ch=operations.Register_for_prod('prod.json',prod_id,n)
-                        f44=open('prod.json','r')
-                        c44=str(json.load(f44))
-                        if prod_id not in c44:
-                            print("Invalid prod ID")
-                        if ch==True:
-                            print("Successfully Registered")
+                    elif i==2:
+                        print('Enter Product Id:')
+                        pd_id = input()
+                        print('1. Add Item to wishlist')
+                        print('2. Remove item from wishlist')
+                        j = int(input())
+                        l=operations.Manage_Wishlist(n,'gamers.json',j,pd_id)
+                        if l:
+                            print('Updated Succesfully')
                         else:
-                            print("prod seats are full! \n")
+                            print('Try again')
+                        
                     elif i==3:
-                        print("Enter new password")
-                        pswd=input()
-                        if(len(pswd))<4:
-                            print("Please enter valid data")
-                            continue
-                        op=operations.Update_Password('gamers.json',n,pswd)
-                        if op==True:
-                            print("Password updated successfully")
+                        print('1. Add Item to Cart')
+                        print('2. Remove item from Cart')
+                        print('3. View Cart')
+                        j = int(input())
+                        print('Enter Product Id:')
+                        pd_id = input()
+                        print('Enter Quantity:')
+                        pd_quantity = input()
+                        print('Enter Booking Startdate:')
+                        pd_st = input()
+                        print('Enter Booking Enddate:')
+                        pd_end = input()
+                        x = open('Products.json')
+                        content = json.load(x)
+                        for i in range(len(content)):
+                            if content[i]["ProductID"] == pd_id:
+                                pd_price = int(content[i]["Price Per Day"])
+                        l=operations.Manage_Cart(n,'gamers.json',j,pd_id,pd_quantity,pd_st,pd_end,pd_price)
+                            
+                        if l:
+                            print('Updated Succesfully')
                         else:
-                            print("Cannot update password")
+                            print('Try again')
+                        
                     elif i==4:
-                        print("Enter prod ID")
-                        ev_id=input()
-                        f14=open('prod.json','r')
-                        try:
-                            c14=str(json.load(f14))
-                            if ev_id not in c14:
-                                print("Invalid prod ID")
-                                continue
-                        except JSONDecodeError:
-                            print("prods not available")
-                            continue
-                        d=operations.View_prod_ByID('prod.json',ev_id)
-                        print("prod Name: " +str(d[0]['Name']))
-                        print("Seller: "+str(d[0]["seller"]))
-                        print("Seats Available: "+str(d[0]['Seats Available']))
-                        print('\n')
+                        l = operations.Place_order(n,'gamers.json','order.json')
+                        if l:
+                            print('order place succesfully')
+                        else:
+                            print('Order not placed')
+                    elif i==5:
+                        print("Enter the detail you want to update Email || Password")
+                        data = input()
+                        print("Enter the new data")
+                        new_data = input()
+                        l = operations.Update_Details(n,'gamers.json',data,new_data)
+                        if l:
+                            print('Updated')
+                        else:
+                            print('Unsuccesful')
+                    elif i==6:
+                        f = open('order.json')
+                        content = json.load(f)
+                        for i in range(len(content)):
+                            if content[i]["Gamer"]==n:
+                                print(content[i])
                     elif i==0:
                         break
                     else:
@@ -298,4 +293,3 @@ while True:
     else:
         print("Invalid Choice, Please enter again")
         continue
-
